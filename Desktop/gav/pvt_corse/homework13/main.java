a4e07f14dacf487f9342e011f8343611a95d90e0
maksim <maksim.katser@appcraft.by>
2019-02-16 13:15:03 +0300

    Add homework13

---
+++ Desktop/gav/pvt_corse/homework13/main.java
@@ -0,0 +1,18 @@
+package homework13;
+public class main {
+	
+	public static void main(String[] args) {
+Massive massive = new Massive();
+massive.DivZero();
+massive.NolimitDivZer();
+massive.FileNotFoundDivZer();
+massive.NullPoint();
+
+
+
+
+
+
+	}
+
+}
