63eaedd55706b0675f39905df64ec6b92daea1b3
maksim <maksim.katser@appcraft.by>
2019-03-02 08:14:11 +0300

    change hometask19

--- hmw19/BookingTest.java
+++ hmw19/BookingTest.java
@@ -32,7 +32,6 @@ public class BookingTest {
 
 	private WebDriver driver;
 
-	
 	@Test
 	public void CheapFilter() {
 		WebDriver driver = new ChromeDriver();
@@ -115,7 +114,6 @@ public class BookingTest {
 
 	}
 
-	
 	@Test
 	public void CheapFromExpensive() {
 		WebDriver driver = new ChromeDriver();
@@ -180,8 +178,7 @@ public class BookingTest {
 		System.out.println("Проскролили");
 
 		int minHighPrice = Integer.parseInt(expFilter.getText().replaceAll("[\\D]", ""));
-		
-		
+
 		System.out.println("ценовой порог: " + minHighPrice);
 
 		expFilter.click();
@@ -279,22 +276,14 @@ public class BookingTest {
 		WebElement hotelButtonTwo = driver.findElement(By.xpath("//div[@class=\"sr-cta-button-row\"][1]/div/div/a[1]"));
 		hotelButtonTwo.click();
 		System.out.println("Тапнули по кнопке");
-		
-		
-
-
-
-
 
 		System.out.println(driver.getCurrentUrl());
 		Set<String> windows = driver.getWindowHandles();
 		for (String winHandle : windows) {
 			driver.switchTo().window(winHandle);
-		}		System.out.println("новая стр");
+		}
+		System.out.println("новая стр");
 		System.out.println(driver.getCurrentUrl());
-		
-		
-		
 
 		WebElement booking = driver
 				.findElement(By.xpath("//div[@id='wrap-hotelpage-top']//button[@id='hp_book_now_button']"));
@@ -307,12 +296,12 @@ public class BookingTest {
 			e.printStackTrace();
 		}
 		System.out.println("Опять спать");
-		
+
 		WebElement numberHuman = driver.findElement(By.xpath("//select[@class=\"hprt-nos-select\"][1]"));
 		Select numberSelect = new Select(numberHuman);
 		numberSelect.selectByIndex(1);
 		System.out.println("Выбрали кол-во людей");
-		
+
 		WebElement bookingExactly = driver.findElement(By.xpath("//div[@class=\"hprt-reservation-cta\"]/button"));
 		bookingExactly.click();
 		System.out.println("Тапнули по забронировать под скролом");
@@ -320,52 +309,53 @@ public class BookingTest {
 		WebElement travelByJob = driver.findElement(By.xpath("//label[@for=\"bp_travel_purpose_business\"]"));
 		travelByJob.click();
 		System.out.println("Тапнули по путешевствую по работе");
-		
+
 		WebElement sex = driver.findElement(By.xpath("//select[@class=\"bp_input_select bp_form__field__input\"]"));
 		Select sexSelect = new Select(sex);
 		sexSelect.selectByIndex(1);
 		System.out.println("г-н");
-		
+
 		WebElement nameField = driver.findElement(By.xpath("//input[@type=\"text\"][1]"));
 		nameField.clear();
 		nameField.sendKeys("Maksim");
 		System.out.println("Ввести 'maksim'");
-		
+
 		WebElement surnameField = driver.findElement(By.xpath("//input[@id=\"lastname\"]"));
 		surnameField.clear();
 		surnameField.sendKeys("kat");
 		System.out.println("Ввести 'kat'");
-		
+
 		WebElement emailField = driver.findElement(By.xpath("//input[@id=\"email\"]"));
-		jse.executeScript("arguments[0].scrollIntoView()", emailField); 
+		jse.executeScript("arguments[0].scrollIntoView()", emailField);
 		System.out.println("Проскролили до поля емэила");
 		emailField.clear();
 		emailField.sendKeys("max_kacer@mail.ru");
 		System.out.println("Ввести 'max_kacer@mail.ru'");
-		
+
 		WebElement emailConfirmField = driver.findElement(By.xpath("//input[@id=\"email_confirm\"]"));
 		emailConfirmField.clear();
 		emailConfirmField.sendKeys("max_kacer@mail.ru");
 		System.out.println("Ввести 'max_kacer@mail.ru'");
-		
+
 		WebElement whoGuest = driver.findElement(By.xpath("//input[@id=\"notstayer_false\"]"));
 		whoGuest.click();
 		System.out.println("я гость");
-		
-		WebElement numberOfGuests = driver.findElement(By.xpath("//select[@class=\"ClickTaleSensitive bp_bs2_guest_dropdown bp_input_select notranslate\"]"));
-		jse.executeScript("arguments[0].scrollIntoView()", numberOfGuests); 
+
+		WebElement numberOfGuests = driver.findElement(
+				By.xpath("//select[@class=\"ClickTaleSensitive bp_bs2_guest_dropdown bp_input_select notranslate\"]"));
+		jse.executeScript("arguments[0].scrollIntoView()", numberOfGuests);
 		System.out.println("Проскролили до селекта гостей");
 		Select guestSelect = new Select(numberOfGuests);
 		guestSelect.selectByIndex(0);
 		System.out.println("Выбрали кол-вао гостей");
-		
+
 		WebElement nameSurnameGuest = driver.findElement(By.xpath("//input[@size=\"22\"][1]"));
 		nameSurnameGuest.clear();
 		nameSurnameGuest.sendKeys("maksim kat");
 		System.out.println("Ввести 'maksim kat'");
-		
+
 		WebElement submitButton = driver.findElement(By.xpath("//button[@type=\"submit\"][1]"));
-		jse.executeScript("arguments[0].scrollIntoView()", submitButton); 
+		jse.executeScript("arguments[0].scrollIntoView()", submitButton);
 		System.out.println("Проскролили до кнопки подтвердить");
 		submitButton.click();
 		System.out.println("клик по подтвердить");
@@ -374,41 +364,41 @@ public class BookingTest {
 		phone.clear();
 		phone.sendKeys("375333911111");
 		System.out.println("Ввести '375333911111'");
-		
+
 		WebElement cc = driver.findElement(By.xpath("//select[@id=\"cc_type\"]"));
-		jse.executeScript("arguments[0].scrollIntoView()", cc); 
+		jse.executeScript("arguments[0].scrollIntoView()", cc);
 		System.out.println("Проскролили до кредитки");
 		Select ccSelect = new Select(cc);
 		ccSelect.selectByIndex(2);
 		System.out.println("Выбрали тип сс");
-		
+
 		WebElement ccNumber = driver.findElement(By.xpath("//input[@name=\"cc_number\"]"));
 		ccNumber.clear();
 		ccNumber.sendKeys("4242 4242 4242 4241");
 		System.out.println("Ввести '4242 4242 4242 4241'");
-		
+
 		WebElement ccYear = driver.findElement(By.xpath("//select[@id=\"ccYear\"]"));
-		jse.executeScript("arguments[0].scrollIntoView()", ccYear); 
+		jse.executeScript("arguments[0].scrollIntoView()", ccYear);
 		System.out.println("Проскролили до кредитки");
 		Select ccSelectYear = new Select(ccYear);
 		ccSelectYear.selectByIndex(1);
 		System.out.println("Выбрали год истечения сс");
-		
+
 		WebElement cvc = driver.findElement(By.xpath("//input[@name=\"cc_cvc\"]"));
 		cvc.clear();
 		cvc.sendKeys("851");
 		System.out.println("cvc 851");
-		
-		WebElement endBook = driver.findElement(By.xpath("//*[@id=\"bookwrapper_cell\"]/div[4]/div[2]/button[2]/ins/span"));
-		jse.executeScript("arguments[0].scrollIntoView()", endBook); 
+
+		WebElement endBook = driver
+				.findElement(By.xpath("//*[@id=\"bookwrapper_cell\"]/div[4]/div[2]/button[2]/ins/span"));
+		jse.executeScript("arguments[0].scrollIntoView()", endBook);
 		System.out.println("Проскролили до кнопки бука");
 		endBook.click();
 		System.out.println("тыкнули по ней");
-		
+
 		WebElement error = driver.findElement(By.xpath("//a[@id=\"js--pay-form__error__anchor_all\"]"));
 		System.out.println("ошибка из-за неправильно введенной карты");
 
-		
 		driver.close();
 		driver.quit();
 
